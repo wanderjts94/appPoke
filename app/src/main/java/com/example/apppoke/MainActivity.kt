d3c19c519aee75b3b9b55927c456565833607f32
wanderjts94 <jhontitosalcedo94@gmail.com>
2024-03-25 12:57:40 -0500

    pokescroll

--- app/src/main/java/com/example/apppoke/MainActivity.kt
+++ app/src/main/java/com/example/apppoke/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.apppoke
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.apppoke.databinding.ActivityMainBinding
 import com.example.apppoke.tetrofit.PokemonApiService
 import com.example.apppoke.tetrofit.PokemonResponse
@@ -17,6 +18,10 @@ class MainActivity : AppCompatActivity() {
     private lateinit var apiRetrofit: Retrofit
     private lateinit var pokemonAdapter:PokemonAdapter
 
+    private var offsrt=0
+    private var linit=20
+    private var  puedecargar= false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -28,6 +33,27 @@ class MainActivity : AppCompatActivity() {
         binding.rvLista.layoutManager=GridLayoutManager(
             applicationContext,3
         )
+        binding.rvLista.addOnScrollListener(object : RecyclerView.OnScrollListener(){
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                super.onScrolled(recyclerView, dx, dy)
+                if(dy>0){
+                    val itemsvisibles= binding.rvLista.layoutManager!!.childCount
+                    val itemslocales= binding.rvLista.layoutManager!!.itemCount
+                    val primeritemvisible=(binding.rvLista.layoutManager!! as GridLayoutManager)
+                            .findFirstCompletelyVisibleItemPosition()
+                if(puedecargar){
+                    if(itemsvisibles + primeritemvisible >= itemslocales){
+                        puedecargar=false
+                        offsrt+=20
+                        obtenerPokemonRetrifit()
+                    }
+
+                }
+
+                }
+            }
+
+        })
         binding.rvLista.adapter=pokemonAdapter
         obtenerPokemonRetrifit()
 
@@ -35,12 +61,13 @@ class MainActivity : AppCompatActivity() {
 
     private fun obtenerPokemonRetrifit() {
         var service = apiRetrofit.create(PokemonApiService::class.java)
-        var pokemonResponse = service.obtenerpokemon()
+        var pokemonResponse = service.obtenerpokemon(offsrt, linit)
         pokemonResponse.enqueue(object : Callback<PokemonResponse>{
             override fun onResponse(
                 call: Call<PokemonResponse>,
                 response: Response<PokemonResponse>
             ) {
+                puedecargar=true
                 pokemonAdapter.postPokemones(
                     response.body()!!.results)
             }
